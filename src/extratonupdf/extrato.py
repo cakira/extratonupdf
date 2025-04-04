7569cb29725403d63c35d2d139a668977263f06c
Akira - Cleber Akira Nakandakare <cakira@cpqd.com.br>
2025-04-04 17:18:41 -0300

    Extract the categories from the PDF #2

--- src/extratonupdf/extrato.py
+++ src/extratonupdf/extrato.py
@@ -7,6 +7,7 @@
 
 import datetime
 import re
+import zlib
 
 import pymupdf
 
@@ -34,16 +35,25 @@ _MONTHS = {
 
 
 def pdf_to_table(filename) -> list[tuple]:
-    text: str = _get_text_from_pdf(filename)
+    """Extract transactions with dates, descriptions, values and categories
+    from PDF statement."""
+    doc = pymupdf.open(filename)
+    text: str = _get_text_from_pdf(doc)
     year = _discover_year(text)
     table = _get_entries(text, year)
-    return table
+    category_list = _get_categories(doc)
+    # TODO: Raise error is len(category_list) != len()
+    new_table = [
+        (date, title, value, category)
+        for (date, title,
+             value), category in zip(table, category_list, strict=True)
+    ]
+    return new_table
 
 
-def _get_text_from_pdf(filename) -> str:
+def _get_text_from_pdf(pymupdf_document) -> str:
     text: str = ''
-    doc = pymupdf.open(filename)
-    for page in doc:
+    for page in pymupdf_document:
         text = text + page.get_text()
     return text
 
@@ -69,3 +79,67 @@ def _get_entries(extrato_text: str, year: int) -> list[tuple]:
             value = -value
         table += [(date, title, value)]
     return table
+
+
+def _get_categories(pymupdf_document) -> list[str]:
+    # Mapping of image checksums to category names for transaction
+    # classification.
+    # These hashes correspond to small embedded icons in the PDF statement:
+    # - Generated using zlib.adler32() of the raw image bytes.
+    #   See _lightweight_hash()
+    # - Manually mapped through empirical analysis of the PDF format
+    # - Specific to the current statement layout (may need updating if bank
+    #   changes icons)
+    # The empty string represents transactions without a category icon
+    category_hashtable = {
+        '7784104a': '',
+        'fc279303': 'eletrônicos',
+        '11d5189d': 'casa',
+        '8b3caae0': 'lazer',
+        'cfeed549': 'outros',
+        '00fd45bb': 'restaurante',
+        '29637e8d': 'saúde',
+        'cc8dae95': 'serviços',
+        'c6ed8a46': 'supermercado',
+        '5c8d338d': 'transporte',
+        '5a84907b': 'viagem',
+    }
+
+    xrefs = _get_categories_as_xrefs(pymupdf_document)
+    xrefs_to_hashes_dict = _get_hashes_by_xref(xrefs, pymupdf_document)
+    hashes = [xrefs_to_hashes_dict[xref] for xref in xrefs]
+    categories = []
+    for image_hash in hashes:
+        category = category_hashtable.get(image_hash, '?')
+        categories.append(category)
+    return categories
+
+
+def _get_categories_as_xrefs(pymupdf_document) -> list[int]:
+    _TRANSACTIONS_START_PAGE = 4  # The page where transactions start appearing
+
+    categories_xrefs = []
+    for page_num in range(_TRANSACTIONS_START_PAGE, len(pymupdf_document)):
+        page = pymupdf_document.load_page(page_num)
+        images_info = page.get_image_info(xrefs=True)
+        # The first image_info is not related to the categories
+        for info in images_info[1:]:
+            xref = info['xref']
+            categories_xrefs.append(xref)
+    return categories_xrefs
+
+
+def _get_hashes_by_xref(xrefs, pymupdf_document) -> dict[int, str]:
+    xrefs_to_hashes = dict()
+    for xref in set(xrefs):
+        image_bytes = pymupdf_document.extract_image(xref)['image']
+        image_hash = _lightweight_hash(image_bytes)
+        xrefs_to_hashes[xref] = image_hash
+    return xrefs_to_hashes
+
+
+def _lightweight_hash(bytes) -> str:
+    '''The returned hash doesn't need to be tampering resistant or very
+    collision proof, because they are not confidential and there will be only a
+    dozen of categories'''
+    return format(zlib.adler32(bytes), '08x')
